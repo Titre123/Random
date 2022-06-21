1c81d43e78ca1be225c173856455286adbaed178
Ola-Balogun Taiwo <99520815+Titre123@users.noreply.github.com>
2022-06-21 15:31:46 +0100

    Create calculator.js

---
+++ Javascript/calculator.js
@@ -0,0 +1,20 @@
+function addition(num_1,num_2){
+  return num_1 + num_2
+}
+
+function subtraction(num_1,num_2){
+  return num_1 - num_2
+}
+
+function multiplication(num_1,num_2){
+  return num_1 * num_2
+}
+
+function division(num_1,num_2){
+  return num_1 / num_2
+}
+
+console.log(addition(3,4))
+console.log(subtraction(3,4))
+console.log(multiplication(3,4))
+console.log(division(4,3))
